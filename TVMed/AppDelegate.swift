9e0f6b49af5b6de59683dd66d0d66edadc4f2ef5
Vinicius Albino <viniciusmouralbino@gmail.com>
2017-06-05 23:19:27 -0300

    first commit

---
+++ TVMed/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TVMed
+//
+//  Created by Vinicius Albino on 05/06/17.
+//  Copyright © 2017 Vinicius Albino. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TVMed/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TVMed
+//
+//  Created by Vinicius Albino on 05/06/17.
+//  Copyright © 2017 Vinicius Albino. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
